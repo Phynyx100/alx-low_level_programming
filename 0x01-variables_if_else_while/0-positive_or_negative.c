f1be612acf1f53369b3f4cde8f415e91341e5b6a
Phynyx100 <simisoe101@outlook.com>
2023-08-11 09:50:05 -0700

    prints posite, zero or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Determines if a number is positive, zero or negative.
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	strand(time(0);
+			n = rand() - RAND_MAX / 2;
+			if (n > 0)
+			{
+			print("%d is positive\n", n);
+			}
+			else if (n == 0)
+			{
+			print("%d is zero\n", n);
+			}
+			else
+			{
+			print("%d is negative\n", n);
+			}
+			return (0);
+}
