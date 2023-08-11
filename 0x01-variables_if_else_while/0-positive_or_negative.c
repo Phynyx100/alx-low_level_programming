a274b9cb94e5bb4e89d4830f39ebe8401919d90a
Phynyx100 <simisoe101@outlook.com>
2023-08-11 10:23:37 -0700

    syntax correction

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,19 +10,19 @@ int main(void)
 {
 	int n;
 
-	strand(time(0);
+	srand(time(0));
 			n = rand() - RAND_MAX / 2;
 			if (n > 0)
 			{
-			print("%d is positive\n", n);
+			printf("%d is positive\n", n);
 			}
 			else if (n == 0)
 			{
-			print("%d is zero\n", n);
+			printf("%d is zero\n", n);
 			}
 			else
 			{
-			print("%d is negative\n", n);
+			printf("%d is negative\n", n);
 			}
 			return (0);
 }
