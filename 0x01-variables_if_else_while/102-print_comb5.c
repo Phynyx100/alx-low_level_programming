a38d34c69d4ed12387fb15529b43f50939a31fa0
Phynyx100 <simisoe101@outlook.com>
2023-08-14 16:57:09 -0700

    prints all possible combinations of two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two two-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int h;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (h = 0; h < 100; h++)
+		{
+			if (i < h)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((h / 10) + 48);
+				putchar((h % 10) + 48);
+				if (i != 98 || h != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
